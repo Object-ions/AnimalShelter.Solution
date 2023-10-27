26d3a851792b49c778401bd3aedfa611a6da5c10
object-ions <moshikoatia@gmail.com>
2023-10-27 16:10:38 -0700

    Updating files for building web API

--- AnimalShelterApi/Program.cs
+++ AnimalShelterApi/Program.cs
@@ -1,22 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Updated code below!
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else 
+{
+  app.UseHttpsRedirection();
+}
 
 app.UseAuthorization();
 
