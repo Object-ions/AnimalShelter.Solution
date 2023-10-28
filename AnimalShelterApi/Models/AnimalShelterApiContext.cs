6b00b84fa7d4eb6d9b40d72bfd2ab2e187cda67b
object-ions <moshikoatia@gmail.com>
2023-10-27 19:30:07 -0700

    Fix indentation

--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -21,16 +21,16 @@ namespace AnimalShelterApi.Models
         new Dog { DogId = 4, Name = "Luna", Sex = "Female", Age = 4, Personality = "Shy" },
         new Dog { DogId = 5, Name = "Buddy", Sex = "Male", Age = 22, Personality = "Protective" },
         new Dog { DogId = 6, Name = "Max", Sex = "Male", Age = 22, Personality = "Friendly" }
-    );
-builder.Entity<Cat>()
-    .HasData(
+      );
+      builder.Entity<Cat>()
+        .HasData(
         new Cat { CatId = 1, Name = "Whiskers", Sex = "Male", Age = 7, Personality = "Independent" },
         new Cat { CatId = 2, Name = "Oliver", Sex = "Female", Age = 10, Personality = "Affectionate" },
         new Cat { CatId = 3, Name = "Charlie", Sex = "Male", Age = 2, Personality = "Adventurous" },
         new Cat { CatId = 4, Name = "Daisy", Sex = "Female", Age = 4, Personality = "Lazy" },
         new Cat { CatId = 5, Name = "Buddy", Sex = "Male", Age = 22, Personality = "Mysterious" },
         new Cat { CatId = 6, Name = "Oliver", Sex = "Male", Age = 22, Personality = "Playful" }
-    );
+      );
     }
   }
 }
