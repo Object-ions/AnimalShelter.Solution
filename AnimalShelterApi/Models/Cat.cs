5c5eb468ca46557b7f5b26c7fc3e50f16e436bb3
object-ions <moshikoatia@gmail.com>
2023-10-28 12:39:25 -0700

    Add modl validations

--- AnimalShelterApi/Models/Cat.cs
+++ AnimalShelterApi/Models/Cat.cs
@@ -5,7 +5,8 @@ namespace AnimalShelterApi.Models
   public class Cat
   {
     public int CatId { get; set; }
-    [Required (ErrorMessage = "Cat must have a name")]
+    [Required]
+    [StringLength(20, MinimumLength = 2, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
     public string Name { get; set; }
     [Required (ErrorMessage = "Cat must have a sex")]
     public string Sex { get; set; }
--- AnimalShelterApi/Models/Dog.cs
+++ AnimalShelterApi/Models/Dog.cs
@@ -6,10 +6,12 @@ namespace AnimalShelterApi.Models
   {
     public int DogId { get; set; }
     [Required]
+    [StringLength(20, MinimumLength = 2, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.")]
     public string Name { get; set; }
-    [Required]
+    [Required (ErrorMessage = "Dog must have a sex")]
     public string Sex { get; set; }
     [Required]
+    [Rage (0, 240, ErrorMessage = "Age must be between 0 to 240 (months)")]
     public int Age { get; set; }
     public string Personality { get; set; }
   }
