8a1d7add9fb30b0497de932328b1ea20922593e6
object-ions <moshikoatia@gmail.com>
2023-10-28 09:50:32 -0700

    Add model validation

--- AnimalShelterApi/Models/Cat.cs
+++ AnimalShelterApi/Models/Cat.cs
@@ -5,11 +5,12 @@ namespace AnimalShelterApi.Models
   public class Cat
   {
     public int CatId { get; set; }
-    [Required]
+    [Required (ErrorMessage = "Cat must have a name")]
     public string Name { get; set; }
-    [Required]
+    [Required (ErrorMessage = "Cat must have a sex")]
     public string Sex { get; set; }
     [Required]
+    [Rage (0, 240, ErrorMessage = "Age must be between 0 to 240 (months)")]
     public int Age { get; set; }
     public string Personality { get; set; }
   }
